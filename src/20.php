686f0abbf8352d5f9e0881d0fa4aee4ac6c25f06
guedeaarledge773 <guedeaarledge773@gmail.com>
2025-03-31 18:19:34 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,13 @@
+<?php
+// This is an example PHP code to demonstrate basic functionalities.
+// It can be replaced with any specific project or task you wish to accomplish.
+
+// Task: Calculate the sum of two numbers
+echo "Sum of 5 and 3: " . (5 + 3) . "\n";
+
+// Task: Print a random number between 1 and 10
+echo "Random number between 1 and 10: " . rand(1, 10) . "\n";
+
+// Task: Display the current date and time
+echo "Current date and time: " . date("Y-m-d H:i:s") . "\n";
+?>
