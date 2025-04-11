09c17cc873099681b3f8ae5e50970a3a89998f80
guedeaarledge773 <guedeaarledge773@gmail.com>
2025-04-11 11:00:19 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,9 @@
+<?php
+// This is an example PHP code for generating a random string.
+$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$randomString = '';
+for ($i = 0; $i < 10; $i++) {
+    $randomString .= $characters[rand(0, strlen($characters) - 1)];
+}
+echo "Random String: " . $randomString;
+?>
